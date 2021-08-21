b1a2ff7b6ed17a32c07e84674cfe94607cbb8b36
Bagus Merta <merta.bagus24@gmail.com>
2021-08-21 21:49:40 +0800

    refactor(global): fix bugs and implement best practice

--- internal/products/controller/product_router.go
+++ internal/products/controller/product_router.go
@@ -11,7 +11,7 @@ func ProductRouter(r *mux.Router, db *sql.DB) {
 	pm := &model.ProductModel{DB: db}
 	r.HandleFunc("/api/product/insert", InsertProductController(pm)).Methods("POST", "OPTIONS")
 	r.HandleFunc("/api/product/getAll", GetAllProductController(pm)).Methods("GET", "OPTIONS")
-	r.HandleFunc("/api/product/delete", DeleteProductController(pm)).Methods("POST", "OPTIONS")
-	r.HandleFunc("/api/product/getSingle", GetSingleProductController(pm)).Methods("POST", "OPTIONS")
-	r.HandleFunc("/api/product/update", UpdateProductController(pm)).Methods("POST", "OPTIONS")
+	r.HandleFunc("/api/product/delete", DeleteProductController(pm)).Methods("DELETE", "OPTIONS")
+	r.HandleFunc("/api/product/getSingle", GetSingleProductController(pm)).Methods("GET", "OPTIONS")
+	r.HandleFunc("/api/product/update", UpdateProductController(pm)).Methods("PUT", "OPTIONS")
 }
